dd814f208fe626bc6808bbb021a6b609b30b2afb
francescobergonzini <francesco.bergonzini@webion.it>
2024-12-29 17:57:40 +0100

    feat: cleanup directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,12 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["YoungSerif"],
+        sans: ["Outfit"]
+      }
+    }
+  }
 } satisfies Config;
